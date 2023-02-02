e48b557c0883083427d2a5576c8b806071262769
Igor Barroso Almeida <igorba867@gmail.com>
2023-02-02 17:26:03 -0300

    del button

--- MochilaViagem/js/main.js
+++ MochilaViagem/js/main.js
@@ -7,6 +7,51 @@ itens.forEach( (elemento) => {
     exibeItemLista(elemento);
 });
 
+// Adiciona novo item a lista ao clicar em submit
+form.addEventListener("submit", (evento) => {
+    // Tira o comportamento padrão do evento de submit
+    evento.preventDefault();
+
+    // Pega os values do formulario a partir do identificador
+    const nome = evento.target.elements["nome"];
+    const quantidade = evento.target.elements["quantidade"];
+    
+    // Cria objeto e o adiciona a lista
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    // Trata de casos onde o item já está na lista
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+
+    if(existe) {
+        // O id do novo item é igual ao que já existia
+        itemAtual.id = existe.id;
+
+        atualizaItem(itemAtual);
+
+        itens[itens.findIndex(elemento => elemento === existe.id)] = itemAtual;
+    } else{
+        // Id do novo item é igual ao tamanho do vetor de itens
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1].id) + 1 : 0;
+
+        // O item é colocado na lista de itens
+        itens.push(itemAtual);
+
+        // Exibe no documento html
+        exibeItemLista(itemAtual);
+    }
+
+    // Adiciona a lista transformada em string ao localStorage
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    // Limpa form
+    nome.value = "";
+    quantidade.value = "";
+})
+
+
 // Função que exibe um item passado como parametro
 function exibeItemLista(item) {
     // Cria item item da lista (<li></li>)
@@ -16,37 +61,42 @@ function exibeItemLista(item) {
     // Cria elemento strong que contem a quantidade (<strong>quantidade</strong>)
     const quantidadeItem = document.createElement("strong");
     quantidadeItem.innerHTML = item.quantidade;
+    // Seta um id na tag strong
+    quantidadeItem.dataset.id = item.id;
 
-    // Adiciona quantidade (como elemento filho) e o nome ao novo item
+    // Adiciona quantidade e o butão de delete (como elementos filhos), e o nome ao novo item
     novoItem.appendChild(quantidadeItem);
+    novoItem.innerHTML += item.nome;
+    novoItem.appendChild(createDelButton(item.id));
 
     // Adiciona item na lista
-    novoItem.innerHTML += item.nome;
     lista.appendChild(novoItem);
 }
 
-// Adiciona novo item a lista ao clicar em submit
-form.addEventListener("submit", (evento) => {
-    // Tira o comportamento padrão do evento de submit
-    evento.preventDefault();
+function atualizaItem(item) {
+    const quantidadeAntiga = document.querySelector("[data-id='"+item.id+"']");
 
-    // Pega os values do formulario a partir do identificador
-    const nome = evento.target.elements["nome"];
-    const quantidade = evento.target.elements["quantidade"];
+    quantidadeAntiga.innerHTML = item.quantidade;
+}
 
-    // Cria objeto e o adiciona a lista
-    const itemAtual = {
-        "nome": nome.value,
-        "quantidade": quantidade.value
-    }
-    itens.push(itemAtual);
+function createDelButton(id) {
+    const elementoButton = document.createElement("button");
+    elementoButton.innerHTML = "X";
+    elementoButton.classList.add("delButton");
 
-    exibeItemLista(itemAtual);
+    elementoButton.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    })
 
-    // Adiciona a lista transformada em string ao localStorage
+    return elementoButton;
+}
+
+function deletaElemento(elemento, id) {
+    elemento.remove();
+
+    // Procura elemento com id passado e tira ele da lista
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    // Atualiza localStorage
     localStorage.setItem("itens", JSON.stringify(itens));
-
-    // Limpa form
-    nome.value = ""
-    quantidade.value = ""
-})
+}
